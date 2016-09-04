d2016108a7655d226d290321e174fbb796ec31e6
archae0pteryx <jesus.christ@openmailbox.org>
2016-09-04 16:03:22 -0700

    refining

---
+++ carret.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+len=23
+pos=0
+score=0
+str="$(tr -Cd 'A-Za-z0-9' < /dev/urandom | head -c "$len" | sed -e 's/./& /g')"
+
+show_caret() {
+    tput el1
+    head -c $((2*pos)) /dev/zero | tr '\0' ' '
+    echo '^'
+}
+
+echo "${str}"
+show_caret
+
+while (( $pos < $len )); do
+    read -rsN1 ch
+    if [[ $ch ==  "${str:$((2*pos)):1}" ]]; then
+        ((score++))
+        ((pos++))
+        tput cuu1
+        show_caret
+      else
+        echo "eerrrnnnnt!"
+        echo "Your score is $1", $score
+        exit 0
+    fi
+done
